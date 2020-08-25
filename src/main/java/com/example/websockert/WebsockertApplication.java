57b68e87ee02ea38c0fd208cdaa56b54b169e2a2
yuanjian <18672793611@163.com>
2020-08-25 16:10:06 +0800

    Initial commit

---
+++ src/main/java/com/example/websockert/WebsockertApplication.java
@@ -0,0 +1,13 @@
+package com.example.websockert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsockertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websockert/WebsockertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websockert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
